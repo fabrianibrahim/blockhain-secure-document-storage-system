6a4ed767c7992ac2820e0da9d40a587a08eb6306
Fabrian Ibrahim <fabrian.ibrahim@gmail.com>
2024-05-19 18:51:26 +1000

    remove secret keys

--- secure-doc-storage-system-frontend/src/INFURA_IPFS_IDS.js
+++ secure-doc-storage-system-frontend/src/INFURA_IPFS_IDS.js
@@ -1,6 +1,6 @@
-let PROJECT_ID="fa78eb428988401d84b20018c0b3e23e"
-let PROJECT_SECRET="Qg8z7oehm6xUY5k/1S6uG0Dla7Jhkhm7xLOoGeBCypVSH0G7OiPyeg"
-let SUB_DOMAIN="https://mainnet.infura.io/v3/fa78eb428988401d84b20018c0b3e23e"
+let PROJECT_ID=""
+let PROJECT_SECRET=""
+let SUB_DOMAIN=""
 module.exports = {
     PROJECT_ID,
     PROJECT_SECRET,
